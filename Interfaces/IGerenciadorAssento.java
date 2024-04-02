e1ccba01bf58b0e97c302f7ddad978fec1ed992f
Livia Canuto <canutto.livia@gmail.com>
2024-04-01 21:21:10 -0300

    Comentario IGerenciadorAssento.java

--- Interfaces/IGerenciadorAssento.java
+++ Interfaces/IGerenciadorAssento.java
@@ -4,6 +4,7 @@ import Entidades.Assento;
 import Entidades.Pessoa;
 
 //Interface Segregation
+//Open - Closed
 public interface IGerenciadorAssento {
     public boolean marcar(Assento assento, Pessoa passageiro);
     public boolean cancelar(Assento assento);
